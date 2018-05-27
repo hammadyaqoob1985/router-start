aa81750a26d58eae18f02430838fc76da24f1e1d
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-05-27 17:03:31 +0100

    made more dynamic by passign id and information to components via router links

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params} from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-server',
@@ -9,11 +11,20 @@ import { ServersService } from '../servers.service';
 })
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
-
-  constructor(private serversService: ServersService) { }
+  serverParamsSubscription : Subscription;
+  serverId: number;
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+   const id = +this.route.snapshot.params['id'];
+     this.server = this.serversService.getServer(id);
+
+    this.serverParamsSubscription = this.route.params
+    .subscribe(
+      (params : Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      } 
+    );
   }
 
 }
